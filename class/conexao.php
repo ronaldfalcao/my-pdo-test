f0b4bff2639959f66893b4da470af8ab4a6a55ea
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2015-06-28 15:25:04 -0300

    Iniciando a classe de conexão

---
+++ class/conexao.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ronald
+ * Date: 28/06/2015
+ * Time: 15:12
+ */
+
+require '../config-database/config-database.php';
+
+class conexao
+{
+
+    public function conectar(){
+
+    }
+
+}
